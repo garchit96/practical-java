079f75e4de6f34b20ed545b7802993a8ed4455a3
Archit <garchit96@gmail.com>
2021-06-21 15:37:56 +0530

    CarAPI
    get RandomCar

--- src/main/java/com/course/practical/java/api/server/CarApi.java
+++ src/main/java/com/course/practical/java/api/server/CarApi.java
@@ -1,8 +1,12 @@
 package com.course.practical.java.api.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +15,17 @@ import com.course.practical.java.service.CarService;
 @RestController
 @RequestMapping(value="/api/car/vi")
 public class CarApi {
+	
+	private static final Logger LOG=LoggerFactory.getLogger(CarApi.class);
+	
 	@Autowired
 	private CarService carService;
 	@GetMapping(value="/random", produces=MediaType.APPLICATION_JSON_VALUE)
 	public Car random() {
 		return carService.generateCar();
 	}
+	@PostMapping(value="/echo", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public String echo(@RequestBody Car car) {
+		LOG.info("Car is {}", car);
+		return car.toString();	}
 }
--- src/main/java/com/course/practical/java/entity/Car.java
+++ src/main/java/com/course/practical/java/entity/Car.java
@@ -5,6 +5,10 @@ public class Car {
 	private String brand;
 	private String color;
 	private String type;
+	
+	public Car() {
+		
+	}
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
